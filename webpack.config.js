e72b915909f73d090c4a9fb47788452e5b2601fb
Daniel <tchatchouang.ouokam@gmail.com>
2022-04-03 18:09:08 +0200

    add additional config

--- webpack.config.js
+++ webpack.config.js
@@ -1,137 +1,136 @@
-const path = require('path');
+const path = require('path')
 const webpack = require('webpack')
 
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
 
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
 
 const dirApp = path.join(__dirname, 'app')
 const dirShared = path.join(__dirname, 'shared')
 const dirStyles = path.join(__dirname, 'styles')
-const dirNode = "node_modules"
+const dirNode = 'node_modules'
 module.exports = {
-    entry: [
-        path.join(dirApp, 'index.js'),
-        path.join(dirStyles, 'index.scss')
-    ],
+  entry: [
+    path.join(dirApp, 'index.js'),
+    path.join(dirStyles, 'index.scss')
+  ],
 
-    resolve : {
-        modules : [
-            dirApp,
-            dirShared,
-            dirStyles,
-            dirNode
+  resolve: {
+    modules: [
+      dirApp,
+      dirShared,
+      dirStyles,
+      dirNode
+    ]
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      IS_DEVELOPMENT
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './shared',
+          to: ''
+        }
+      ]
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+
+    new ImageMinimizerPlugin({
+      minimizer: {
+        implementation: ImageMinimizerPlugin.imageminMinify,
+        options: {
+          // Lossless optimization with custom option
+          // Feel free to experiment with options for better result for you
+          plugins: [
+            ['gifsicle', { interlaced: true }],
+            ['jpegtran', { progressive: true }],
+            ['optipng', { optimizationLevel: 5 }]
+          ]
+        }
+      }
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: ''
+            }
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
         ]
-    },
+      },
 
-
-    plugins : [
-        new webpack.DefinePlugin({
-            IS_DEVELOPMENT
-        }),
-
-        new CopyWebpackPlugin({
-            patterns : [
-                {
-                    from : './shared',
-                    to : ''
-                }
-            ]
-        }),
-
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename : '[id].css'
-        }),
-
-        new ImageMinimizerPlugin({
-            minimizer: {
+      {
+        test: /\.(jpe?g|png|gif|svg|wof2?|fnt|webp)$/,
+        loader: 'file-loader',
+        options: {
+          name (file) {
+            return '[hash].[ext]'
+          }
+        }
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|webp)$/i,
+        use: [
+          {
+            loader: ImageMinimizerPlugin.loader,
+            options: {
+              minimizer: {
                 implementation: ImageMinimizerPlugin.imageminMinify,
                 options: {
-                  // Lossless optimization with custom option
-                  // Feel free to experiment with options for better result for you
                   plugins: [
-                    ["gifsicle", { interlaced: true }],
-                    ["jpegtran", { progressive: true }],
-                    ["optipng", { optimizationLevel: 5 }],
-                  ],
-                },
+                    'imagemin-gifsicle',
+                    'imagemin-mozjpeg',
+                    'imagemin-pngquant',
+                    'imagemin-svgo'
+                  ]
+                }
               }
-        })
-    ],
-
-    module : {
-        rules : [
-            {
-                test : /\.js$/,
-                use : {
-                    loader : 'babel-loader',
-                }
-            },
-
-            {
-                test : /\.scss$/,
-                use: [
-                    {
-                        loader : MiniCssExtractPlugin.loader,
-                        options : {
-                            publicPath : ''
-                        }
-                    },
-                    {
-                        loader : 'css-loader',
-                    },
-                    {
-                        loader : 'postcss-loader',
-                    },
-                    {
-                        loader : 'sass-loader'
-                    }
-                ]
-            },
-
-            {
-                test : /\.(jpe?g|png|gif|svg|wof2?|fnt|webp)$/,
-                loader : 'file-loader',
-                options : {
-                    name(file){
-                        return '[hash].[ext]'
-                    }
-                }
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg|webp)$/i,
-                use: [
-                  {
-                    loader: ImageMinimizerPlugin.loader,
-                    options: {
-                      minimizer: {
-                        implementation: ImageMinimizerPlugin.imageminMinify,
-                        options: {
-                          plugins: [
-                            "imagemin-gifsicle",
-                            "imagemin-mozjpeg",
-                            "imagemin-pngquant",
-                            "imagemin-svgo",
-                          ],
-                        },
-                      },
-                    },
-                  },
-                ],
-            },
-            {
-                test : /\.(glsl|frag|vert)$/,
-                loader : 'raw-loader',
-                exclude : /node_modules/
-            },
-            {
-                test : /\.(glsl|frag|vert)$/,
-                loader : 'glslify-loader',
-                exclude: /node_modules/
             }
+          }
         ]
-    }
+      },
+      {
+        test: /\.(glsl|frag|vert)$/,
+        loader: 'raw-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(glsl|frag|vert)$/,
+        loader: 'glslify-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
 }

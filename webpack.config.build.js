d930e74c434bfb42f502c0211fd9898036c4f3ec
Daniel <tchatchouang.ouokam@gmail.com>
2022-04-03 18:12:52 +0200

    save sass as dev-dependency

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,18 +1,18 @@
 const path = require('path')
 
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
-const {merge}=require('webpack-merge')
+const { merge } = require('webpack-merge')
 const config = require('./webpack.config')
 
-module.exports = merge(config,{
-    mode : 'production',
-    
-    output : {
-        path : path.join(__dirname,'public')
-    },
+module.exports = merge(config, {
+  mode: 'production',
 
-    plugins : [
-        new CleanWebpackPlugin()
-    ]
+  output: {
+    path: path.join(__dirname, 'public')
+  },
+
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 })
--- webpack.config.development.js
+++ webpack.config.development.js
@@ -1,17 +1,17 @@
-const {merge} = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const path = require('path')
 
 const config = require('./webpack.config')
 
 module.exports = merge(config, {
-    mode : 'development',
-    devtool : 'inline-source-map',
-    devServer : {
-        devMiddleware: {
-            writeToDisk : true,
-        }
-    },
-    output : {
-        path : path.resolve(__dirname, 'public')
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    devMiddleware: {
+      writeToDisk: true
     }
+  },
+  output: {
+    path: path.resolve(__dirname, 'public')
+  }
 })
